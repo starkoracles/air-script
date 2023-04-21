19089fa558fd90f7c4e501c5f89d40e4b28375d2
skaller <skaller@users.sourceforge.net>
2023-04-21 12:47:48 +1000

    Generated Cairo now compiles.

--- codegen/cairo0/src/lib.rs
+++ codegen/cairo0/src/lib.rs
@@ -24,176 +24,106 @@ pub struct CodeGenerator {
 }
 
 impl CodeGenerator {
-    // --- CONSTRUCTOR ----------------------------------------------------------------------------
+  // --- CONSTRUCTOR ----------------------------------------------------------------------------
 
-    /// Builds a new Rust scope that represents a Cairo0 Air trait implementation for the
-    /// provided AirIR.
-    pub fn new(_ir: &AirIR) -> CodeGenerator {
-        Self {
-            air_name: _ir.air_name.clone(),
-            segment_widths: _ir.declarations.trace_segment_widths().to_vec(),
-            constants: _ir.declarations.constants().to_vec(),
-            public_inputs: _ir.declarations.public_inputs().to_vec(), //Vec<(String, usize)>
-            periodic_columns: _ir.declarations.periodic_columns().to_vec(), //Vec<Vec<u64>>`
-            boundary_constraints: _ir.constraints.boundary_constraints.clone(), //Constraints
-            integrity_constraints: _ir.constraints.integrity_constraints.clone(), //Constraints
-            graph: _ir.constraints.graph.clone(),
-        }
+  /// Builds a new Rust scope that represents a Cairo0 Air trait implementation for the
+  /// provided AirIR.
+  pub fn new(_ir: &AirIR) -> CodeGenerator {
+    Self {
+      air_name: _ir.air_name.clone(),
+      segment_widths: _ir.declarations.trace_segment_widths().to_vec(),
+      constants: _ir.declarations.constants().to_vec(),
+      public_inputs: _ir.declarations.public_inputs().to_vec(), //Vec<(String, usize)>
+      periodic_columns: _ir.declarations.periodic_columns().to_vec(), //Vec<Vec<u64>>`
+      boundary_constraints: _ir.constraints.boundary_constraints.clone(), //Constraints
+      integrity_constraints: _ir.constraints.integrity_constraints.clone(), //Constraints
+      graph: _ir.constraints.graph.clone(),
     }
+  }
 
-    pub fn showvalue(&self, x: &Value) -> String {
-        match x {
-            Value::BoundConstant(_) => "BoundConstant".to_string(),
-            Value::InlineConstant(v) => v.to_string(),
-            Value::TraceElement(ita) => {
-                let offset = &ita.row_offset();
-                let colidx = &ita.col_idx().to_string();
-                if *offset == 0 {
-                    "cur[".to_string() + &colidx + "]"
-                } else {
-                    "nxt[".to_string() + &offset.to_string() + "]"
-                }
-            }
-            Value::PeriodicColumn(_, _) => "PeriodicColumn".to_string(),
-            Value::PublicInput(_, _) => "PublicInput".to_string(),
-            Value::RandomValue(_) => "RandomValue".to_string(),
+  pub fn showvalue(&self, x: &Value) -> String {
+    match x {
+      Value::BoundConstant(_) => "BoundConstant".to_string(),
+      Value::InlineConstant(v) => v.to_string(),
+      Value::TraceElement(ita) => {
+        let offset = &ita.row_offset();
+        let colidx = &ita.col_idx().to_string();
+        if *offset == 0 {
+          "cur[".to_string() + &colidx + "]"
+        } else {
+          "nxt[".to_string() + &offset.to_string() + "]"
         }
+      }
+      Value::PeriodicColumn(index, length) => "periodic[".to_string() + &index.to_string() + " + mod(row, " + &length.to_string() + ")" + "]",
+      Value::PublicInput(_, index) => "punlic[".to_string() + "]",
+      Value::RandomValue(x) => "rand[".to_string() + &x.to_string() + "]",
     }
+  }
 
-    pub fn ascairo(&self, w: &NodeIndex) -> String {
-        let op = &self.graph.node(w).op;
-        match op {
-            Operation::Value(x) => self.showvalue(x),
-            Operation::Add(a, b) => {
-                "(".to_string() + &self.ascairo(a) + " + " + &self.ascairo(b) + ")"
-            }
-            Operation::Sub(a, b) => {
-                "(".to_string() + &self.ascairo(a) + " - " + &self.ascairo(b) + ")"
-            }
-            Operation::Mul(a, b) => {
-                "(".to_string() + &self.ascairo(a) + " * " + &self.ascairo(b) + ")"
-            }
-            Operation::Exp(a, j) => {
-                "exp(".to_string() + &self.ascairo(a) + ", " + &j.to_string() + ")"
-            }
-        }
+  pub fn ascairo(&self, w: &NodeIndex) -> String {
+    let op = &self.graph.node(w).op;
+    match op {
+      Operation::Value(x) => self.showvalue(x),
+      Operation::Add(a, b) => "(".to_string() + &self.ascairo(a) + " + " + &self.ascairo(b) + ")",
+      Operation::Sub(a, b) => "(".to_string() + &self.ascairo(a) + " - " + &self.ascairo(b) + ")",
+      Operation::Mul(a, b) => "(".to_string() + &self.ascairo(a) + " * " + &self.ascairo(b) + ")",
+      Operation::Exp(a, j) => "exp(".to_string() + &self.ascairo(a) + ", " + &j.to_string() + ")",
     }
+  }
 
-    pub fn showconvalue(&self, x: &Value) -> String {
-        match x {
-            Value::BoundConstant(_) => todo!(),
-            Value::InlineConstant(v) => v.to_string(),
-            Value::TraceElement(ita) => {
-                "TraceElement(".to_string()
-                    + &ita.col_idx().to_string()
-                    + &{
-                        let offset = &ita.row_offset();
-                        if *offset == 0 {
-                            "".to_string()
-                        } else {
-                            "+".to_string() + &offset.to_string()
-                        }
-                    }
-                    + ")"
-            }
-            Value::PeriodicColumn(_, _) => "PeriodicColumn".to_string(),
-            Value::PublicInput(_, _) => "PublicInput".to_string(),
-            Value::RandomValue(_) => "RandomValue".to_string(),
-        }
-    }
+  /// Returns a string of Cairo code implementing Cairo0
+  pub fn generate(&self) -> String {
+    // header
+    let mut s = 
+      "// Air name ".to_string() + &self.air_name + " " + &(self.segment_widths.len().to_string()) + " segments\n"
+     ;
+     s = s + "from starkware.cairo.common.alloc import alloc\n";
+     s = s + "from starkware.cairo.common.memcpy import memcpy\n";
+     s = s + "\n";
+     s = s + "func exp (x:felt, t:felt) -> felt {\n  return (1); \n}\n";
+     s = s + "func mod(x:felt,y:felt) -> felt {\n  return (1); \n}\n";
+   
+     s = s +
+       "struct EvaluationFrame {\n" +
+       "  current_len: felt,\n" +
+       "  current: felt*,\n" +
+       "  next_len: felt,\n" +
+       "  next: felt*,\n" +
+       "  row: felt,\n" + 
+       "}\n"
+     ;
 
-    pub fn showcon(&self, w: &NodeIndex) -> String {
-        let op = &self.graph.node(w).op;
-        match op {
-            Operation::Value(x) => self.showconvalue(x),
-            Operation::Add(a, b) => {
-                "Add(".to_string() + &self.showcon(a) + ", " + &self.showcon(b) + ")"
-            }
-            Operation::Sub(a, b) => {
-                "Sub(".to_string() + &self.showcon(a) + ", " + &self.showcon(b) + ")"
-            }
-            Operation::Mul(a, b) => {
-                "Mul(".to_string() + &self.showcon(a) + ", " + &self.showcon(b) + ")"
-            }
-            Operation::Exp(a, j) => {
-                "Exp(".to_string() + &self.showcon(a) + ", " + &j.to_string() + ")"
-            }
-        }
-    }
+     // Each segment
+     for (i, w) in self.segment_widths.iter().enumerate() {
+       s = s + "\n// SEGMENT " + &i.to_string() + " size " + &w.to_string() + "\n" + 
+         "// ===============================================\n"
+       ;
+       s = s + 
+         "func evaluate_transition_" + &i.to_string() + " (\n" + 
+         "  frame: EvaluationFrame,\n" + 
+         "  t_evaluations: felt*,\n" + 
+         "  periodic: felt*,\n" +                       // periodic value vector FIXME: DESIGN FAULT!
+         { if i > 0 { "  rand: felt*,\n" } else { "" }} + 
+         ") {\n" + 
+         "  alloc_locals;\n" + 
+         "  let cur = frame.current;\n" + 
+         "  let nxt = frame.next;\n" + 
+         "  let row = frame.row;\n"
+       ;
+            
+       // just validity constraints
+       let vc = &self.integrity_constraints[i];
+       //s = s + "\n  // Integrity   constraints (" + &(vc.len().to_string()) + ")\n  // ----------------\n";
+       for (i, w) in vc.iter().enumerate() {
+         //s = s + "    // #" + &i.to_string() + ": root node " + &w.index.0.to_string() + " Domain: " + &w.domain.to_string() + "\n";
+         s = s + "  assert t_evaluations[" + &i.to_string() + "] = " + &self.ascairo(&w.index) + ";\n";
+       } // constraints
 
-    /// Returns a string of Cairo code implementing Cairo0
-    pub fn generate(&self) -> String {
-        let mut s1 = "// Hello, Cairo0!\n".to_string()
-            + "// Air name "
-            + &self.air_name
-            + " "
-            + &(self.segment_widths.len().to_string())
-            + " segments\n";
-        s1 = s1 + "from starkware.cairo.common.alloc import alloc\n";
-        s1 = s1 + "from starkware.cairo.common.memcpy import memcpy\n";
-        s1 = s1
-            + "struct EvaluationFrame {\n"
-            + "  current_len: felt,\n"
-            + "  current: felt*,\n"
-            + "  next_len: felt,\n"
-            + "  next: felt*,\n"
-            + "}\n";
-        let mut s2 = "// ".to_string();
-        for (i, w) in self.segment_widths.iter().enumerate() {
-            s2 = s2 + "Segment " + &i.to_string() + "  size " + &w.to_string() + "\n// ";
-        }
-        let mut s3 = "".to_string();
-        for (i, w) in self.segment_widths.iter().enumerate() {
-            s3 = s3
-                + "// SEGMENT "
-                + &i.to_string()
-                + " size "
-                + &w.to_string()
-                + "\n"
-                + "// ===============================================\n";
-            let bc = &self.boundary_constraints[i];
-            s3 = s3
-                + "\n  // Boundary   constraints ("
-                + &(bc.len().to_string())
-                + ") \n  // ----------------\n";
-            for (i, w) in bc.iter().enumerate() {
-                s3 = s3
-                    + "    // #"
-                    + &i.to_string()
-                    + ": root node "
-                    + &w.index.0.to_string()
-                    + " Domain: "
-                    + &w.domain.to_string()
-                    + "\n";
-                let opdisp = &self.showcon(&w.index);
-                s3 = s3 + "    //    " + opdisp + "\n";
-            }
+       s = s + "\n  return ();\n";
+       s = s + "}\n";
 
-            let vc = &self.integrity_constraints[i];
-            s3 = s3
-                + "\n  // Integrity   constraints ("
-                + &(vc.len().to_string())
-                + ")\n  // ----------------\n";
-            for (i, w) in vc.iter().enumerate() {
-                s3 = s3
-                    + "    // #"
-                    + &i.to_string()
-                    + ": root node "
-                    + &w.index.0.to_string()
-                    + " Domain: "
-                    + &w.domain.to_string()
-                    + "\n";
-                let opdisp = &self.showcon(&w.index);
-                s3 = s3 + "    //   " + opdisp + "\n";
-                s3 = s3
-                    + "    assert t_evalations["
-                    + &i.to_string()
-                    + "] = "
-                    + &self.ascairo(&w.index)
-                    + ";\n";
-            }
-        }
+     } // segments
 
-        return s1 + &s2 + &s3 + "\n";
-    }
-}
+     return s + "\n";
+  } // generate
+} // CodeGenerator
--- examples/example.cairo
+++ examples/example.cairo
@@ -1,58 +1,54 @@
-// Hello, Cairo0!
 // Air name ExampleAir 2 segments
 from starkware.cairo.common.alloc import alloc
 from starkware.cairo.common.memcpy import memcpy
+
+func exp (x:felt, t:felt) -> felt {
+  return (1); 
+}
+func mod(x:felt,y:felt) -> felt {
+  return (1); 
+}
 struct EvaluationFrame {
   current_len: felt,
   current: felt*,
   next_len: felt,
   next: felt*,
+  row: felt,
 }
-// Segment 0  size 4
-// Segment 1  size 1
-// // SEGMENT 0 size 4
+
+// SEGMENT 0 size 4
 // ===============================================
+func evaluate_transition_0 (
+  frame: EvaluationFrame,
+  t_evaluations: felt*,
+  periodic: felt*,
+) {
+  alloc_locals;
+  let cur = frame.current;
+  let nxt = frame.next;
+  let row = frame.row;
+  assert t_evaluations[0] = (exp(cur[0], 2) - cur[0]);
+  assert t_evaluations[1] = ((periodic[0 + mod(row, 8)] * (nxt[1] - cur[0])) - 0);
+  assert t_evaluations[2] = (((1 - cur[0]) * ((cur[3] - cur[1]) - cur[2])) - 0);
+  assert t_evaluations[3] = ((cur[0] * (cur[3] - (cur[1] * cur[2]))) - 0);
 
-  // Boundary   constraints (6) 
-  // ----------------
-    // #0: root node 2 Domain: the first row
-    //    Sub(TraceElement(1), PublicInput)
-    // #1: root node 5 Domain: the first row
-    //    Sub(TraceElement(2), PublicInput)
-    // #2: root node 8 Domain: the first row
-    //    Sub(TraceElement(3), PublicInput)
-    // #3: root node 10 Domain: the last row
-    //    Sub(TraceElement(1), PublicInput)
-    // #4: root node 12 Domain: the last row
-    //    Sub(TraceElement(2), PublicInput)
-    // #5: root node 14 Domain: the last row
-    //    Sub(TraceElement(3), PublicInput)
+  return ();
+}
 
-  // Integrity   constraints (4)
-  // ----------------
-    // #0: root node 20 Domain: every row
-    //   Sub(Exp(TraceElement(0), 2), TraceElement(0))
-    assert t_evalations[0] = (exp(cur[0], 2) - cur[0]);
-    // #1: root node 26 Domain: every frame of 2 consecutive rows
-    //   Sub(Mul(PeriodicColumn, Sub(TraceElement(0+1), TraceElement(0))), 0)
-    assert t_evalations[1] = ((PeriodicColumn * (nxt[1] - cur[0])) - 0);
-    // #2: root node 31 Domain: every row
-    //   Sub(Mul(Sub(1, TraceElement(0)), Sub(Sub(TraceElement(3), TraceElement(1)), TraceElement(2))), 0)
-    assert t_evalations[2] = (((1 - cur[0]) * ((cur[3] - cur[1]) - cur[2])) - 0);
-    // #3: root node 35 Domain: every row
-    //   Sub(Mul(TraceElement(0), Sub(TraceElement(3), Mul(TraceElement(1), TraceElement(2)))), 0)
-    assert t_evalations[3] = ((cur[0] * (cur[3] - (cur[1] * cur[2]))) - 0);
 // SEGMENT 1 size 1
 // ===============================================
+func evaluate_transition_1 (
+  frame: EvaluationFrame,
+  t_evaluations: felt*,
+  periodic: felt*,
+  rand: felt*,
+) {
+  alloc_locals;
+  let cur = frame.current;
+  let nxt = frame.next;
+  let row = frame.row;
+  assert t_evaluations[0] = (nxt[1] - (cur[0] * (cur[3] + rand[0])));
 
-  // Boundary   constraints (1) 
-  // ----------------
-    // #0: root node 17 Domain: the first row
-    //    Sub(TraceElement(0), 1)
-
-  // Integrity   constraints (1)
-  // ----------------
-    // #0: root node 40 Domain: every frame of 2 consecutive rows
-    //   Sub(TraceElement(0+1), Mul(TraceElement(0), Add(TraceElement(3), RandomValue)))
-    assert t_evalations[0] = (nxt[1] - (cur[0] * (cur[3] + RandomValue)));
+  return ();
+}
 
